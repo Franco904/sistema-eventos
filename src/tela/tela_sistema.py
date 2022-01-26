ea3bdf1e315eb512d77252ac6fe9d0580b43cbf1
francostavares <francostavares2003@gmail.com>
2022-01-26 18:00:19 -0300

    Cria diretórios + ControladorSistema & TelaSistema (rascunho)

---
+++ src/tela/tela_sistema.py
@@ -0,0 +1,9 @@
+class TelaSistema:
+    # Fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+
+    # TODO: Implementar opções da tela inicial
+    def tela_opcoes(self):
+        print("-------- Sistema de Eventos ---------")
+        print("(...)")
+        opcao = int(input("Escolha a opcao: "))
+        return opcao
