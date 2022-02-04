c4d37fb7d201553a784a9f61a17deae61b948f2f
francostavares <francostavares2003@gmail.com>
2022-02-04 09:40:16 -0300

    Ajustes em participante/endereco

--- src/entidade/participante.py
+++ src/entidade/participante.py
@@ -1,9 +1,9 @@
-from datetime import datetime
-from pessoa import Pessoa
-from endereco import Endereco
+from datetime import date
+
 from comprovante_saude import ComprovanteSaude
+from endereco import Endereco
 from enums.status_participante import StatusParticipante
-from enums.resultado_pcr import ResultadoPcr
+from pessoa import Pessoa
 
 
 class Participante(Pessoa):
@@ -12,20 +12,24 @@ class Participante(Pessoa):
         if isinstance(data_nascimento[0], int) \
                 and isinstance(data_nascimento[1], int) \
                 and isinstance(data_nascimento[2], int):
-            self.__data_nascimento = datetime(data_nascimento[0], data_nascimento[1], data_nascimento[2], 0, 0)
+
+            if 0 <= (date.today().year - data_nascimento[0]) <= 150:
+                self.__endereco = Endereco(endereco[0], endereco[1], endereco[2])
+                self.__data_nascimento = date(data_nascimento[0], data_nascimento[1], data_nascimento[2])
+                self.__status_participante = StatusParticipante.a_confirmar
+                self.__comprovante_saude = None
+            else:
+                raise TypeError
         else:
             raise TypeError
-        self.__endereco = Endereco(endereco[0], endereco[1], endereco[2])
-        self.__status = StatusParticipante.a_confirmar
-        self.__comprovante_saude = ComprovanteSaude(False, False, [2022, 3, 2, 19, 52], ResultadoPcr.nao_realizado)
 
     @property
     def data_nascimento(self):
         return self.__data_nascimento
 
     @property
-    def status(self):
-        return self.__status
+    def status_participante(self):
+        return self.__status_participante
 
     @property
     def comprovante_saude(self):
@@ -40,14 +44,14 @@ class Participante(Pessoa):
         if isinstance(data_nascimento[0], int) \
                 and isinstance(data_nascimento[1], int) \
                 and isinstance(data_nascimento[2], int):
-            self.__data_nascimento = datetime(data_nascimento[0], data_nascimento[1], data_nascimento[2], 0, 0)
+            self.__data_nascimento = date(data_nascimento[0], data_nascimento[1], data_nascimento[2])
         else:
             raise TypeError
 
-    @status.setter
-    def status(self, nome: str):
-        if isinstance(nome, str):
-            self.__nome = nome
+    @status_participante.setter
+    def status_participante(self, status_participante: StatusParticipante):
+        if isinstance(status_participante, StatusParticipante):
+            self.__status_participante = status_participante
         else:
             raise TypeError
 
@@ -56,8 +60,6 @@ class Participante(Pessoa):
         self.__endereco = Endereco(endereco[0], endereco[1], endereco[2])
 
     @comprovante_saude.setter
-    def comprovante_saude(self, status_participante: StatusParticipante):
-        if isinstance(status_participante, StatusParticipante):
-            self.__status_participante = status_participante
-        else:
-            raise TypeError
+    def comprovante_saude(self, comprovante_saude: list):
+        self.__comprovante_saude = ComprovanteSaude(comprovante_saude[0], comprovante_saude[1],
+                                                    comprovante_saude[2], comprovante_saude[3])
