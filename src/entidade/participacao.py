0038139250a6a41c91e92058ac8d9995ca92d42c
Jean Luca Pacheco <jeanlucapacheco@gmail.com>
2022-02-07 00:21:21 -0300

    Correções Entidade Participação

--- src/entidade/participacao.py
+++ src/entidade/participacao.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 
-from src.entidade.participante import Participante
-
 
 class Participacao:
-    def __init__(self, id: int, id_evento: int, data_horario_entrada: list, data_horario_saida: list,
-                 participante: Participante):
+    def __init__(self, id: int, id_evento: int, data_horario_entrada: list,
+                 cpf_participante: str):
         if isinstance(id, int) \
                 and isinstance(id_evento, int) \
                 and isinstance(data_horario_entrada[0], int) \
@@ -13,12 +11,7 @@ class Participacao:
                 and isinstance(data_horario_entrada[2], int) \
                 and isinstance(data_horario_entrada[3], int) \
                 and isinstance(data_horario_entrada[4], int) \
-                and isinstance(data_horario_saida[0], int) \
-                and isinstance(data_horario_saida[1], int) \
-                and isinstance(data_horario_saida[2], int) \
-                and isinstance(data_horario_saida[3], int) \
-                and isinstance(data_horario_saida[4], int) \
-                and isinstance(participante, Participante):
+                and isinstance(cpf_participante, str):
             self.__id = id
             self.__id_evento = id
             self.__data_horario_entrada = datetime(data_horario_entrada[0],
@@ -26,12 +19,8 @@ class Participacao:
                                                    data_horario_entrada[2],
                                                    data_horario_entrada[3],
                                                    data_horario_entrada[4])
-            self.__data_horario_saida = datetime(data_horario_saida[0],
-                                                 data_horario_saida[1],
-                                                 data_horario_saida[2],
-                                                 data_horario_saida[3],
-                                                 data_horario_saida[4])
-            self.__participante = participante
+            self.__data_horario_saida = None
+            self.__cpf_participante = cpf_participante
         else:
             raise TypeError
 
@@ -52,8 +41,8 @@ class Participacao:
         return self.__data_horario_saida
 
     @property
-    def participante(self):
-        return self.__participante
+    def cpf_participante(self):
+        return self.__cpf_participante
 
     @id.setter
     def id(self, id: int):
@@ -99,9 +88,9 @@ class Participacao:
         else:
             raise TypeError
 
-    @participante.setter
-    def participante(self, participante: Participante):
-        if isinstance(participante, Participante) and participante is not None:
-            self.__participante = participante
+    @cpf_participante.setter
+    def cpf_participante(self, cpf_participante: str):
+        if isinstance(cpf_participante, str):
+            self.__cpf_participante = cpf_participante
         else:
             raise TypeError
